4032a980018dff632aef769103132f425fbb534d
naggit2026 <awsdataengineer2026@gmail.com>
2025-01-24 21:26:06 +0530

    lambda code is modified

--- lambda_function.py
+++ lambda_function.py
@@ -2,13 +2,13 @@ import json
 import requests
 import pandas as pd
 
-def lambda_handler(event,context):
-    print('event data ->', event)
-    response=response.get("https://www.google.com/")
+def lambda_handler(event, context):
+    
+    print("Event Data -> ", event)
+    response = requests.get("https://www.google.com/")
     print(response.text)
 
-    d={'col1':[1,2,3],'col2':[4,5,6]}
-    df=pd.DataFrame(data=d)
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
     print(df)
-    print('Done x1.1')
-    print('demo completed !!!')
+    print('Demo Completed !!!')
